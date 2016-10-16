d613a2ca48dd0eb87f0f1e758e30404d594c6006
Blake Lawson <blakemlaws@gmail.com>
2016-10-16 15:41:48 -0400

    This should probably have been divided across several commits, but this commit reorganized files so that the go modules are arranged as golang expects.
    Also modified topo.py so that it performs a real test. While performing
    these modifications a number of changes were made to the test VM so
    that mininet gets executed properly and tcpdump has proper permissions.
    
    Main code changes occur in client.go. At this point, the client sends
    the first SYN packet of TCP handshake.

---
+++ src/main/client.go
@@ -0,0 +1,10 @@
+// Blake Lawson (blawson@princeton.edu)
+// Adviser: Jennifer Rexford
+
+package main
+
+import "client"
+
+func main() {
+  client.Start()
+}
---
+++ src/main/proxy.go
@@ -0,0 +1,10 @@
+// Blake Lawson (blawson@princeton.edu)
+// Adviser: Jennifer Rexford
+
+package main
+
+import "proxy"
+
+func main() {
+  proxy.Start()
+}
